d0ea3a6c7cef83439faea841de2e32e99ee28ee7
SakaiTaka23 <tt3.sakai@gmail.com>
2022-10-11 21:18:37 +0900

    リポジトリ実装

---
+++ gateways/repository/article/repo.go
@@ -0,0 +1,41 @@
+package article
+
+import (
+	"hex-grpc/entities"
+	"hex-grpc/usecase/repository"
+
+	"gorm.io/gorm"
+)
+
+type article struct {
+	conn *gorm.DB
+}
+
+func New(conn *gorm.DB) repository.Article {
+	return &article{}
+}
+
+func (a *article) Create(article entities.Article) string {
+	a.conn.Create(&article)
+	return article.ID
+}
+
+func (a *article) Delete(ID string) {
+	a.conn.Delete(entities.Article{}, ID)
+}
+
+func (a *article) FindOne(ID string) entities.Article {
+	var article entities.Article
+	a.conn.First(&article).Where("id = ?", ID)
+	return article
+}
+
+func (a *article) GetAll() []entities.Article {
+	var articles []entities.Article
+	a.conn.Model(entities.Article{}).Take(&articles)
+	return articles
+}
+
+func (a *article) Update(article entities.Article) {
+	a.conn.Model(&article).Updates(article)
+}
--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require (
 	github.com/golang/protobuf v1.5.2
 	google.golang.org/grpc v1.50.0
 	google.golang.org/protobuf v1.28.1
+	gorm.io/gorm v1.24.0
 )
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
 	golang.org/x/text v0.3.3 // indirect
