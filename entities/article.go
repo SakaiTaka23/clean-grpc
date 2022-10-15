87af7743c2faa5ac2302e698c54554569b645bdd
SakaiTaka23 <tt3.sakai@gmail.com>
2022-10-15 18:59:38 +0900

    作成前のuuid生成

--- entities/article.go
+++ entities/article.go
@@ -1,7 +1,18 @@
 package entities
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Article struct {
 	ID    string
 	Title string
 	Price string
+}
+
+func (a *Article) BeforeCreate(tx *gorm.DB) (err error) {
+	id, _ := uuid.NewUUID()
+	a.ID = id.String()
+	return
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.1.2
 	google.golang.org/grpc v1.50.0
 	google.golang.org/protobuf v1.28.1
 	gorm.io/driver/sqlite v1.4.2
